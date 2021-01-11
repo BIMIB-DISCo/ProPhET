e032a45809a652d47c74673a868ca1f4ac86f5e5
marcoxa <marcoxa@gmail.com>
2021-01-11 18:45:45 +0100

    Now properly formatted.

--- homeoplasia.jl
+++ homeoplasia.jl
@@ -1,28 +1,38 @@
+### -*- Mode: Julia -*-
+
+### homeoplasia.jl
+
 using RData
 using NamedArrays
+
 
 function findSNV(B, C)
     genSample = Dict()
     for i in 2:size(B,1)
-        push!(genSample, i-1 => [k for (k,v) in enamerate(C) if v==i])
+        push!(genSample, i-1 => [k for (k, v) in enumerate(C) if v == i])
     end
     return genSample
 end
 
+
 function buildD(B, C, P)
-    # Get mutation and sample names
-    mutation_name = filter!(e->e!="Root", allnames(B)[2])
+    ## Get mutation and sample names
+    mutation_name = filter!(e->e != "Root", allnames(B)[2])
     sample_name = allnames(C)[1]
 
-    # Build D getting values from P
-    # result = reshape([P[sample,mutation] for sample in allnames(C) for mutation in allnames(B)[2]], (size(C)[1], size(B)[2]))
-    result = reshape([P[sample,mutation] for sample in sample_name for mutation in mutation_name], (length(sample_name), length(mutation_name)))
+    ## Build D getting values from P
+    ## result = reshape([P[sample,mutation] for sample in allnames(C)
+    ## for mutation in allnames(B)[2]], (size(C)[1], size(B)[2]))
+    result = reshape([P[sample,mutation]
+                      for sample in sample_name
+                      for mutation in mutation_name],
+                     (length(sample_name), length(mutation_name))) 
 
-    # Set D colnames and rownames
+    ## Set D colnames and rownames
     result = NamedArray(result, (sample_name, mutation_name))
 
-    # element >= 0.9 -> 1
-    # element <  0.9 -> 0
+    ## element >= 0.9 -> 1
+    ## element <  0.9 -> 0
     function repl(x)
         if ismissing(x)
             return missing
@@ -36,9 +46,10 @@ function buildD(B, C, P)
     return map(x->repl(x), result)
 end
 
+
 function compare(D, G)
-    # compare D (original data) with G (corrected genotypes)
-    total=Dict()
+    ## compare D (original data) with G (corrected genotypes)
+    total = Dict()
     for j in allnames(D)[2]
         fp = 0
         fn = 0
@@ -47,7 +58,7 @@ function compare(D, G)
                 if D[i,j] != G[i,j]
                     if G[i,j] == 0
                         fp += 1 #/size(G)[1]
-                    elseif G[i,j]==1
+                    elseif G[i,j] == 1
                         fn += 1 #/size(G)[1]
                     end
                 end
@@ -59,9 +70,10 @@ function compare(D, G)
     return total
 end
 
+
 function check_HP_violation(total, α, β)
-    # find suspicious mutation given alpha and beta
-    # cand = {}
+    ## find suspicious mutation given alpha and beta
+    ## cand = {}
     for (k,v) in total
         if v[1] >= α
             println("FP: $k")
@@ -73,21 +85,34 @@ function check_HP_violation(total, α, β)
             # println(β)
             println(v[1])
         end
-
     end
 end
 
 
 data = load(ARGS[1], convert=true)
 names = load(ARGS[2], convert=true)["names"]
-P = NamedArray(load(ARGS[3], convert=true)["processed_variants"], (names["P_rows"], names["P_cols"]))
-B = NamedArray(data["inference"]["B"], (names["B_rows"], names["B_cols"]))
-C = NamedArray(data["inference"]["C"]["Experiment_1"][:,1], names["C_rows"])
-G = NamedArray(data["inference"]["corrected_genotypes"], (names["G_rows"], names["G_cols"]))
+
+P = NamedArray(load(ARGS[3], convert = true)["processed_variants"],
+               (names["P_rows"], names["P_cols"]))
+
+B = NamedArray(data["inference"]["B"],
+               (names["B_rows"], names["B_cols"]))
+
+C = NamedArray(data["inference"]["C"]["Experiment_1"][:,1],
+               names["C_rows"])
+
+G = NamedArray(data["inference"]["corrected_genotypes"],
+               (names["G_rows"], names["G_cols"]))
+
 α = data["inference"]["error_rates"]["alpha"]
 β = data["inference"]["error_rates"]["beta"]
 D = buildD(B, C, P)
-# print(findSNV(B,C))
-# print(allnames(buildD(B, C, P)))
+
+## print(findSNV(B, C))
+## print(allnames(buildD(B, C, P)))
 print(compare(D, G))
-# check_HP_violation(compare(D,G), α, β)
+## check_HP_violation(compare(D,G), α, β)
+
+
+### end of file -- homeoplasia.jl
+
